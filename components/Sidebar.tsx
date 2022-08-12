838a42551f353187e76ad685251fba26ccf18abc
naimsiddiquibd <naimsiddiquibd@gmail.com>
2022-08-12 10:22:23 +0600

    Sanity CMS

---
+++ components/Sidebar.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import {
+    BellIcon,
+    HashtagIcon,
+    BookmarkIcon,
+    CollectionIcon,
+    DotsCircleHorizontalIcon,
+    MailIcon,
+    UserIcon,
+    HomeIcon,
+} from '@heroicons/react/outline';
+import SidebarRow from './SidebarRow';
+
+function Sidebar() {
+  return (
+    <div className="flex flex-col col-span-2 items-center px-4 md:items-start">
+        <img className="h-10 w-10 m-3" src="https://links.papareact.com/drq" alt="" />
+        <SidebarRow Icon={HomeIcon} title="Home" />
+        <SidebarRow Icon={HashtagIcon} title="Explore" />
+        <SidebarRow Icon={BellIcon} title="Notification" />
+        <SidebarRow Icon={MailIcon} title="Message" />
+        <SidebarRow Icon={BookmarkIcon} title="Bookmarks" />
+        <SidebarRow Icon={CollectionIcon} title="Lists" />
+        <SidebarRow Icon={DotsCircleHorizontalIcon} title="More" />
+    </div>
+  )
+}
+
+export default Sidebar
---
+++ components/Widgets.tsx
@@ -0,0 +1,27 @@
+import { SearchIcon } from '@heroicons/react/outline'
+import React from 'react'
+import { TwitterTimelineEmbed } from 'react-twitter-embed';
+
+function Widgets() {
+  return (
+    <div className='px-2 mt-2 col-span-2 hidden md:inline'>
+        {/* Search */}
+        <div className="flex items-center space-x-2 bg-gray-100 p-3 rounded-full mt-2">
+            <SearchIcon className="h-5 w-5 text-gray-400" />
+            <input
+            type="text"
+            placeholder="Search what you want"
+            className="flex-1  outline-none bg-transparent " />
+        </div>
+
+        <TwitterTimelineEmbed
+        sourceType="profile"
+        screenName="naimsiddiquibd"
+        options={{height: 1000}}
+        />
+
+    </div>
+  )
+}
+
+export default Widgets
